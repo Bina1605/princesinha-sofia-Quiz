9dd6e19f7cddb1df1164564758338f37a18e7e0f
Bina1605 <00001132614776sp@al.educacao.sp.gov.br>
2024-12-07 00:11:05 -0300

    Add files via upload

---
+++ Projeto Bina/perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'Princesinha Sofia',
+    'Quem é o melhor amigo da Princesinha Sofia?',
+    'O melhor amigo da Princesinha Sofia é o animalzinho Clover.'
+);
+
+criaCartao(
+    'Princesinha Sofia',
+    'Qual é o nome do reino onde Princesinha Sofia vive?',
+    'A Princesinha Sofia vive no Reino de Enchancia.'
+);
+
+criaCartao(
+    'Princesinha Sofia',
+    'Quem é o padrinho mágico da Princesinha Sofia?',
+    'O padrinho mágico da Princesinha Sofia é o feiticeiro Cedric.'
+);
+
+criaCartao(
+    'Princesinha Sofia',
+    'Qual é o nome da coroa mágica da Princesinha Sofia?',
+    'A coroa mágica da Princesinha Sofia é a Coroa Real.'
+);
